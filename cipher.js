0e5134db6009f4e74a5df103487d2b7362243f7b
andresavieira28 <andresacg_14@hotmail.com>
2023-02-22 17:03:41 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -29,13 +29,17 @@ function decode(offset,text) {
   for (let a = 0; a< text.length; a++){
 
     const numberASCII = text.charCodeAt(a);
+
     //1° condição MAIUSCULA 65-90, 2° MINUSCULA CONDIÇÃO 97-122, 3° CONDIÇÃO CARACTERES 32-64
+
     if(numberASCII >= 65 && numberASCII <= 90) {
       const calcDecode = (((numberASCII + 65) - parseInt(offset)) % 26) + 65;
       msgdec += String.fromCharCode(calcDecode);
+
     } else if(numberASCII >= 97 && numberASCII <= 122) {
-      const calcDecode = (((numberASCII + 97) - parseInt(offset)) % 26) + 97;
+      const calcDecode = (((numberASCII - 97) - parseInt(offset)) % 26) + 97;
       msgdec += String.fromCharCode(calcDecode);
+
     } else if(numberASCII >= 32 || numberASCII <= 64) {
       msgdec += String.fromCharCode(numberASCII);
     }
